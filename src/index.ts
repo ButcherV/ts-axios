9a6470182916005060005883916da3739d2e7487
butcherv <butcherv@foxmail.com>
2021-10-12 14:21:46 +0800

    feat: URL

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 interface LabelledValue {
   label: string
 }
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
