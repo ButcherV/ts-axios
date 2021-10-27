10b5d28ec41c3a368312846140e40e28d7073bdb
butcherv <butcherv@foxmail.com>
2021-10-27 11:55:47 +0800

    feat: Network error

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const { data = null, url, method = 'get', headers, responseType } = config
 
     const request = new XMLHttpRequest()
@@ -31,6 +31,10 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
 
       resolve(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
     }
 
     Object.keys(headers).forEach(name => {
